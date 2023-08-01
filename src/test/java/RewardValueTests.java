ebd11387a3dc3e39771799df45f7ef5c591e673d
jjmahoney6 <140572769+jjmahoney6@users.noreply.github.com>
2023-08-01 13:49:25 -0400

    Code finished

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,7 +1,7 @@
-import org.junit.jupiter.api.Test;
+
+import org.testng.annotations.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class RewardValueTests {
 
     @Test
